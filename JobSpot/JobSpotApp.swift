8c79c136f9f5eb26fda00a320e154c760775365a
Nikhil Barik <nklmarch17@gmail.com>
2025-07-02 18:02:22 +0530

    Initial Commit

---
+++ JobSpot/JobSpotApp.swift
@@ -0,0 +1,17 @@
+//
+//  JobSpotApp.swift
+//  JobSpot
+//
+//  Created by Nikhil Barik on 02/07/25.
+//
+
+import SwiftUI
+
+@main
+struct JobSpotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
